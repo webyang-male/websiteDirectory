38b234941a6608ce34b8e523de38b686714d6048
webyang-male <mr.yangweb@gmail.com>
2022-01-20 21:45:09 +0800

    :pencil: 日常维护与美化更新

--- source/js/custom.js
+++ source/js/custom.js
@@ -1,14 +1,14 @@
 // 网页刷新时判断系统时间，适配深浅模式
 window.onload = function checkTime() {
-	var t = new Date().getHours();
-	if (t >= 18 || t <= 6) {
-		document.body.classList.add('night');
-		document.cookie = "night=1;path=/"
-	} else {
-		document.body.classList.remove('night');
-		document.cookie = "night=0;path=/"
-	}
-}
+  var t = new Date().getHours();
+  if (t >= 18 || t <= 6) {
+    document.body.classList.add("night");
+    document.cookie = "night=1;path=/";
+  } else {
+    document.body.classList.remove("night");
+    document.cookie = "night=0;path=/";
+  }
+};
 
 // 鼠标滑过网址卡片、输入框触发背景模糊 -----------------------------开始
 // 提示：一定要用querySelector来选择，用其它没效果
@@ -19,7 +19,7 @@ window.onload = function checkTime() {
 // var timer = null;
 // var timer1 = null;
 // var blur = 0;
-// var search_btn = document.querySelector('.search-btn') 
+// var search_btn = document.querySelector('.search-btn')
 // 利用循环绑定注册事件
 // for (var i = 0; i < card.length; i++) {
 // 	card[i].onmouseout = function () {
@@ -54,16 +54,26 @@ window.onload = function checkTime() {
 // 卖萌标题
 var OriginTitle = document.title;
 var titleTime;
-document.addEventListener('visibilitychange', function () {
-	if (document.hidden) {
-		$('[rel="icon"]').attr('href', "/img/favicon.ico");
-		document.title = '(つェ⊂) 你脸上有点东西~~';
-		clearTimeout(titleTime);
-	} else {
-		$('[rel="icon"]').attr('href', "/img/favicon.ico");
-		document.title = '(*´∇｀*) 有点可爱~~  ' + OriginTitle;
-		titleTime = setTimeout(function () {
-			document.title = OriginTitle;
-		}, 2000);
-	}
+document.addEventListener("visibilitychange", function () {
+  if (document.hidden) {
+    $('[rel="icon"]').attr("href", "/img/favicon.ico");
+    document.title = "(つェ⊂) 你脸上有点东西~~";
+    clearTimeout(titleTime);
+  } else {
+    $('[rel="icon"]').attr("href", "/img/favicon.ico");
+    document.title = "(*´∇｀*) 有点可爱~~  " + OriginTitle;
+    titleTime = setTimeout(function () {
+      document.title = OriginTitle;
+    }, 2000);
+  }
 });
+
+!(function () {
+  let head = document.head || document.getElementsByTagName("head")[0];
+  if (head != undefined) {
+    head += `<meta name="baidu-site-verification" content="code-FmEoVVRuTT" />`;
+    document.getElementsByTagName("head")[0].innerHTML += head;
+  } else {
+    console.log("head is undefined");
+  }
+})();
